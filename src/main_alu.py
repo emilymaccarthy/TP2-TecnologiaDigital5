a5c9940e9e0469e2e7e33a7a9ddc2d0203dbe47b
Emily Mac Carthy <emilymaccarthy03@gmail.com>
2024-06-11 16:56:26 -0300

    hasta algoritmo e implementacion

--- src/main_alu.py
+++ src/main_alu.py
@@ -12,6 +12,7 @@ def printGraph(G):
 	plt.show()
 
 def addService(service, G):
+    ## haces los nodos
 	id, data = service
 	# print(data)
 	from_station = data["stops"][0]["station"]
@@ -27,6 +28,7 @@ def addService(service, G):
 	# print(service, data["services"][service]["stops"])
 
 def addNodesAndTrainEdges(data, G): 
+    ## agrega los trenes de viaje 
 	services = data["services"]
 	estacion = None
 	for service in services.items():
@@ -40,20 +42,18 @@ def addNodesAndTrainEdges(data, G):
 		flow = int(np.ceil(demand/capacidad))
 
 		print(type(data['services']))
-		G.add_node(from_time,weight=0, demand = flow, color='blue')
-		G.add_node(to_time,weight=0, demand= -flow, color='red')
+		G.add_node(from_time,demand = flow, color='blue')
+		G.add_node(to_time, demand= -flow, color='red')
 		G.add_edge(from_time, to_time, weight= 0,capacity = data["rs_info"]["max_rs"] - flow, color='green' )
 		
 
-
-	pass
 
 # def addTrainEdges(data, G):
 # 	pass
 
 
 def addTraspasoEdges(data, G):
-
+	## conecta dos eventos consecutivos 
 	for station in data["stations"]:
 		station_nodes = []
 
@@ -71,6 +71,7 @@ def addTraspasoEdges(data, G):
 	pass
 
 def getFirstDeparture(estacion, data, G): 
+    ## primer servicio del diaa
 	res = 10000000000000
 	for key, value in data["services"].items():  
 		stops = value["stops"]
@@ -80,6 +81,7 @@ def getFirstDeparture(estacion, data, G):
 	return res
 
 def getLastArrival(estacion, data, G):
+    ## ultimo servico del dia
     res = -1
     for key, value in data["services"].items(): 
         stops = value["stops"]
@@ -89,7 +91,7 @@ def getLastArrival(estacion, data, G):
     return res 
 
 def addTrasNocheEdges(data, G): 
-
+	## agregas las aristas de trasnoche 
 	for estacion in data["stations"]:
 		inicio = getFirstDeparture(estacion, data, G)
 		final = getLastArrival(estacion, data, G)
@@ -114,19 +116,78 @@ def generateGraph(filename:str):
 	# pos = nx.bipartite_layout(G, [node for node in G.nodes if G.nodes[node]['bipartite']==0])
 	return G
 	
+def costo_minimo(flowDict,G):
+    
+    
+    for u, v  in G.edges:
+        if G.edges[u,v]['color'] == 'green':
+            print(u, v,  G.nodes[u]["demand"] )
+            print(f"flowdict[u,v] before: {flowDict[u][v]}")
+            flowDict[u][v] += G.nodes[u]["demand"] 
+            print(f"flowdict[u,v] after: {flowDict[u][v]}")
+            
+    print(flowDict)
+    
+def vagones_totales(flowDict,filename, G):
+	data = None
+	flujo_estacion = 0
 
+	with open(filename) as json_file:
+		data = json.load(json_file)
+		json_file.close()
+  
+	for estacion in data["stations"]:
+		inicio = getFirstDeparture(estacion, data, G)
+		final = getLastArrival(estacion, data, G)
+		flujo_estacion = flowDict[final][inicio]
+  
+		print(F"{estacion}: {flujo_estacion} vagones")
+	
+    
+    
+def vagones_inciales(G, flowDict, estaciones):
+	flujo_estacion1 = 0
+	flujo_estacion2 = 0
+    
+    #para las aristas de trasnoche
+    
+	for u,v in G.edges:
+     
+		if G.edges[u,v]['color'] == 'red':
+			#actualiza flujos
+			if estaciones[0] in u:
+				if u in flowDict and v in flowDict[u]:
+        
+					flujo_estacion1 += flowDict[u][v]
+			elif estaciones[1] in u:
+				if u in flowDict and v in flowDict[u]:
+					flujo_estacion2 += flowDict[u][v]
+     
+	print(F"{estaciones[0]}: {flujo_estacion1} vagones")
+	print(F"{estaciones[1]}: {flujo_estacion2} vagones")
+				
+					
+            
 	
 def main():
-	TOY = True
-	if(TOY):
+	instance = 1
+	if(instance == 0):
 		filename = "instances/toy_instance.json"
+	elif(instance == 1):
+		filename = "instances/instance_2.json"
 	else:
 		filename = "instances/retiro-tigre-semana.json"
 
 	G = generateGraph(filename)
+	printGraph(G)
+
 	flowDict = nx.min_cost_flow(G)
-	print(flowDict)
-	# printGraph(G)
+	costo_minimo(flowDict,G)
+ 
+	estaciones = ['Retiro','Tigre']
+
+	vagones_totales(flowDict, filename, G)
+	
 
 	
 
